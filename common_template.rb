3dc8aca3d1c88449b2923b2a8d879d58aa074ac7
msqui <msqui@yandex.ru>
2011-12-31 15:47:07 +0400

    common template enhanced

--- common_template.rb
+++ common_template.rb
@@ -1,57 +1,140 @@
-create_file ".rvmrc", "rvm gemset use #{app_name}"
+create_file ".rvmrc", "rvm gemset use #{app_name} --create"
+run "rvm use 1.9.3@#{app_name}"
 
-gem "haml-rails"
-#gem "sass"
-gem "nifty-generators"
+# ========
+# = Gems =
+# ========
+gem "haml"
 gem "simple_form"
+gem "inherited_resources"
 
 gem "devise"
 gem "cancan"
 
-gem "rails3-generators", :group => [ :development ]
-gem "rspec-rails", :group => [ :development, :test ]
-gem "factory_girl_rails", :group => [ :development, :test ]
-gem "capybara", :group => :test
+gem "sanitize"
+
+gem "haml-rails", group: :development
+gem "nifty-generators", group: :development
+
+gem "pry", group: [:development, :test]
+
+gem "rspec-rails", group: :test
+gem "shoulda-matchers", group: :test
+gem "factory_girl_rails", group: :test
+gem "database_cleaner", group: :test
+gem "capybara", group: :test
+
+gem "compass", "~> 0.12.alpha.2", group: :assets
 
 run 'bundle install'
 
-rake "db:create", :env => 'development'
-rake "db:create", :env => 'test'
+# =================
+# = Configuration =
+# =================
 
-generate 'nifty:layout --haml'
-remove_file 'app/views/layouts/application.html.erb'
-generate 'simple_form:install'
-generate 'nifty:config'
-
-generate 'rspec:install'
-inject_into_file 'spec/spec_helper.rb', "\nrequire 'factory_girl'", :after => "require 'rspec/rails'"
+# generators
 inject_into_file 'config/application.rb', :after => "config.filter_parameters += [:password]" do
-	<<-ruby
+	<<-RUBY
 		config.generators do |g|
 			g.template_engine :haml
 			g.test_framework :rspec, fixture: true, views: false
 			g.fixture_replacement :factory_girl, dir: 'spec/factories'
 		end
-	ruby
+	RUBY
 end
-run "echo '--format documentation' >> .rspec"
 
-generate "devise:install"
-generate "devise:install"
-generate "devise User"
-generate "devise:views"
-run "db:migrate"
-generate "cancan:ability"
+# devise
+model_name = 'User'
+generate 'devise:install'
+genetate 'devise', model_name
+genetate 'devise:views'
+
+# cancan
+genetate 'cancan:ability'
+
+# simple_form
+genetate 'simple_form:install'
+
+# nifty-generators
+genetate 'nifty:config'
+genetate 'nifty:layout --haml'
+remove_file 'app/views/layouts/application.html.erb'
+
+# rspec
+genetate 'rspec:install'
+
+create_file 'spec/support/devise.rb' do
+  <<-RUBY
+    RSpec.configure do |config|
+      config.include Devise::TestHelpers, type: :controller
+    end
+  RUBY
+end
+
+create_file 'spec/support/factory_girl.rb' do
+  <<-RUBY
+    Rspec.configure do |config|
+      config.include Factory::Syntax::Methods
+    end
+  RUBY
+end
+
+create_file 'spec/support/capybara.rb' do
+  <<-RUBY
+    require "capybara/rspec"
+    require "capybara/rails"
+  RUBY
+end
+
+# ActionMailer
+gsub_file 'config/environments/development.rb', /# Don't care if the mailer can't send/, '# ActionMailer Config'
+gsub_file 'config/environments/development.rb', /config.action_mailer.raise_delivery_errors = false/ do
+  <<-RUBY
+    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = false
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default charset: 'utf-8'
+  RUBY
+end
+
+gsub_file 'config/environments/production.rb', /config.active_support.deprecation = :notify/ do
+  <<-RUBY
+    config.active_support.deprecation = :notify
+    
+    config.action_mailer.default_url_options = { host: 'yourhost.com' }
+    
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = false
+    config.action_mailer.default charset: 'utf-8'
+  RUBY
+end
 
 remove_file 'public/index.html'
 remove_file 'rm public/images/rails.png'
+
+generate :controller, 'home index'
+gsub_file 'config/routes.rb', /get \"home\/index\"/, 'root to: "home#index"'
+
+rake "db:create", :env => 'development'
+rake "db:create", :env => 'test'
+
+run "db:migrate"
+
 run 'cp config/database.yml config/database.example'
 run "echo 'config/database.yml' >> .gitignore"
 
+# ==============
+# = Git commit =
+# ==============
 git :init
-git :add => "."
-git :commit => "-a -m 'initial commit'"
-say <<-eos
-  Your app is ready
-eos
+git add: "."
+git commit: "-a -m 'initial commit'"
 
+# =========
+# = Final =
+# =========
+say <<-EOS
+  Your app is ready
+EOS
